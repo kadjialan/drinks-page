84020126779818d6621d948311b04f860a26653e
kadjialan <alankadji7@gmail.com>
2023-02-28 10:36:38 +0100

    fetching from server

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Home from './Home/Home';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [data2, setData2] = useState([]);
+  const BASE2_URL = 'http://localhost:8080/categories/';
+  const BASE_URL = 'http://localhost:8080/drinks/';
+  const API_KEY = '360';
+  useEffect(() => {
+    fetch(`${BASE_URL}?apiKey=${API_KEY}`)
+      .then((res) => res.json())
+      .then((res) => setData([...res]));
+    fetch(`${BASE2_URL}?apiKey=${API_KEY}`)
+      .then((res) => res.json())
+      .then((res) => setData2([...res]));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.js</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Home />
     </div>
   );
 }
